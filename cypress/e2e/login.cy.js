294c96062972d8e7ecf4f0bbf85f44fcf6ecd734
masFreitas <mateusasfreitas@gmail.com>
2023-05-03 15:47:46 -0300

    feat: Login with success (need development)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="cypress"/>
+import loc from '../support/locators'
 
 describe('Login', () => {
     it('Login with success', () => {
         cy.visit('/');
+        cy.xpath(loc.HOME.loginBtn).click();
+        cy.get(loc.LOGIN.emailInput).type(Cypress.env('email'));
+        cy.get(loc.LOGIN.passwordInput).type(Cypress.env('password'));
+
     })
 })
---
+++ cypress/support/locators.js
@@ -0,0 +1,12 @@
+const locators = {
+    HOME: {
+        loginBtn: `//*[@class='fa fa-lock']/..`,
+    },
+    
+    LOGIN: {
+        emailInput: '[data-qa="login-email"]',
+        passwordInput: '[data-qa="login-password"]',
+    }
+}
+
+export default locators
