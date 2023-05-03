ec01327f78e9c4f3f5ce6b6913b8005b796fc536
masFreitas <mateusasfreitas@gmail.com>
2023-05-03 15:47:21 -0300

    feat: add config variables

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationexercise.com/',
+    screenshotOnRunFailure: false,
+    video: false,
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
