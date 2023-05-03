3f5088ed2b4dbbd388f86ef5ac47e83c5f7351cb
masFreitas <mateusasfreitas@gmail.com>
2023-05-03 14:13:34 -0300

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://automationexercise.com/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress"/>
+
+describe('Login', () => {
+    it('Login with success', () => {
+        cy.visit('/');
+    })
+})
