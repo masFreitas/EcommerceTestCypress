d7b61cc538c355a135ed0881e9f99b0282716e28
masFreitas <mateusasfreitas@gmail.com>
2023-05-04 17:14:10 -0300

    feat: login with commands

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,11 +4,7 @@ import loc from '../support/locators'
 describe('Login', () => {
     it('Login with success', () => {
         cy.visit('/');
-        cy.xpath(loc.HOME.loginBtn).click();
-        cy.get(loc.LOGIN.emailInput).type(Cypress.env('email'));
-        cy.get(loc.LOGIN.passwordInput).type(Cypress.env('password'), { log: false });
-        cy.get('[data-qa="login-button"]').click();
-        cy.contains('Delete Account').should('exist');
+        cy.login();
 
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import loc from '../support/locators'
+
+
+Cypress.Commands.add('login', () => {
+    cy.xpath(loc.HOME.loginBtn).click();
+        cy.get(loc.LOGIN.emailInput).type(Cypress.env('email'));
+        cy.get(loc.LOGIN.passwordInput).type(Cypress.env('password'), { log: false });
+        cy.get('[data-qa="login-button"]').click();
+        cy.contains('Delete Account').should('exist');
+})
