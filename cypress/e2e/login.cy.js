57f9697f01499fa72620c66ca32c813f61bffd2d
masFreitas <mateusasfreitas@gmail.com>
2023-05-04 17:11:18 -0300

    feat: Login with success

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,7 +6,9 @@ describe('Login', () => {
         cy.visit('/');
         cy.xpath(loc.HOME.loginBtn).click();
         cy.get(loc.LOGIN.emailInput).type(Cypress.env('email'));
-        cy.get(loc.LOGIN.passwordInput).type(Cypress.env('password'));
+        cy.get(loc.LOGIN.passwordInput).type(Cypress.env('password'), { log: false });
+        cy.get('[data-qa="login-button"]').click();
+        cy.contains('Delete Account').should('exist');
 
     })
 })
