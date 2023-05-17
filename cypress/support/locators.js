adbd4f1de58f8db5f885a42f75258ebcf8a0f288
masFreitas <mateusasfreitas@gmail.com>
2023-05-17 17:20:29 -0300

    feat: create account with success

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -8,6 +8,29 @@ const locators = {
         passwordInput: '[data-qa="login-password"]',
         doLoginBtn: '[data-qa="login-button"]',
     },
+    
+    CREATEACCOUNT: {
+        nameInput: '[data-qa="signup-name"]',
+        emailInput: '[data-qa="signup-email"]',
+        signUpBtn: '[data-qa="signup-button"]',
+        genderBtn: (value) => `//*[@id="id_gender${value}"]`,
+        passwordInput: '[data-qa="password"]',
+        daysSelect: '[data-qa="days"]',
+        monthsSelect: '[data-qa="months"]',
+        yearsSelect: '[data-qa="years"]',
+        addressFirstNameInput: '[data-qa="first_name"]',
+        addressLastNameInput: '[data-qa="last_name"]',
+        companyInput: '[data-qa="company"]',
+        addressInput: '[data-qa="address"]',
+        address2Input: '[data-qa="address2"]',
+        countrySelect: '[data-qa="country"]',
+        stateInput: '[data-qa="state"]',
+        cityInput: '[data-qa="city"]',
+        zipcodeInput: '[data-qa="zipcode"]',
+        mobileNumberInput: '[data-qa="mobile_number"]',
+        createAccountBtn: '[data-qa="create-account"]',
+
+    },
 }
 
 export default locators
