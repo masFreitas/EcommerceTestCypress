f0bbdc84acb51e56fadabf684e190feeb335a9fa
masFreitas <mateusasfreitas@gmail.com>
2023-05-22 15:52:10 -0300

    feat: create account without fill required fields

--- cypress/e2e/createAccount.cy.js
+++ cypress/e2e/createAccount.cy.js
@@ -9,5 +9,14 @@ beforeEach(() => {
 describe('Create account', () => {
     it('Create account with success', () => {
         cy.createAccount();
+    });
+
+    it('Create account without fill required fields', () => {
+        cy.xpath(loc.HOME.loginBtn).click();
+        cy.get(loc.CREATEACCOUNT.nameInput).type(utils.getRandomName());
+        cy.get(loc.CREATEACCOUNT.emailInput).type(utils.getRandomEmail());
+        cy.get(loc.CREATEACCOUNT.signUpBtn).click();
+        cy.get(loc.CREATEACCOUNT.createAccountBtn).click();
+        cy.url().should('contain', '/signup')
     })
 })
